eff30db60b5b6636a21a1f22a3ae4b7354bf66bb
Noemi Yanqui <noemi.yanqui@tecsup.edu.pe>
2019-05-08 16:20:05 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by MAC04 on 8/05/19.
+//  Copyright © 2019 NoemiYanqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
